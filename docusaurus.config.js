1928d561324f2ac314b405c6c2633551b3ad79dd
xiaolang <yyhxiaolang@gmail.com>
2022-10-20 11:14:39 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,13 +6,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'http://cifangbifang.com/blog-graphdocs/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'yyh0808', // Usually your GitHub org/user name.
+  projectName: 'blog-graphdocs', // Usually your repo name.
 
   presets: [
     [
